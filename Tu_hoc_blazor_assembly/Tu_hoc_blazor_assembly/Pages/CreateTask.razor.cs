7bb665ed302b9577d415c50862ab7d29dab2a5d8
Hoàng Phạm Đức <hoang23577@gmail.com>
2023-09-11 05:59:53 +0700

    Create-Form-to-Create-Task

---
+++ Tu_hoc_blazor_assembly/Tu_hoc_blazor_assembly/Pages/CreateTask.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using ToDoList_ViewModel;
+using Tu_hoc_blazor_assembly.Service;
+
+namespace Tu_hoc_blazor_assembly.Pages
+{
+    public partial class CreateTask
+    {
+        [Inject]
+        public IUserAPIClient  UserApiClient { get; set; }
+        [Inject]
+        public ITaskAPIClient TaskAPIClient { get; set; }
+        private TaskCreateRequest CreateRequest { get; set; } = new TaskCreateRequest();
+    }
+}
