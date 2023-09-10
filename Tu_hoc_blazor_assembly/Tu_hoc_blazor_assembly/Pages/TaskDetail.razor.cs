df83d5b95516a50a5c5fb864bfa66e7adde203f1
Hoàng Phạm Đức <hoang23577@gmail.com>
2023-09-10 21:21:09 +0700

    rounting-parameter-detail

---
+++ Tu_hoc_blazor_assembly/Tu_hoc_blazor_assembly/Pages/TaskDetail.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Tu_hoc_blazor_assembly.Pages
+{
+    public partial class TaskDetail
+    {
+        [Parameter]
+        public string TaskId { get; set; }
+
+    }
+}
