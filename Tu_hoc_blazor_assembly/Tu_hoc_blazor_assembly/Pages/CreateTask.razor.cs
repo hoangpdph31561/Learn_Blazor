d520dd8d8a7850069664bb088590af75418c1207
Hoàng Phạm Đức <hoang23577@gmail.com>
2023-09-11 09:51:08 +0700

    Using-Blazor-Toast

--- Tu_hoc_blazor_assembly/Tu_hoc_blazor_assembly/Pages/CreateTask.razor.cs
+++ Tu_hoc_blazor_assembly/Tu_hoc_blazor_assembly/Pages/CreateTask.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using ToDoList_ViewModel;
 using Tu_hoc_blazor_assembly.Service;
@@ -13,13 +14,20 @@ namespace Tu_hoc_blazor_assembly.Pages
         public ITaskAPIClient TaskAPIClient { get; set; }
         [Inject]
         public NavigationManager NavigationManager { get; set; }
+        [Inject]
+        public IToastService ToastService { get; set; }
         private TaskCreateRequest CreateRequest { get; set; } = new TaskCreateRequest();
         private async Task CreateNewTask(EditContext context)
         {
             var result = await TaskAPIClient.CreateTask(CreateRequest);
             if(result)
             {
+                ToastService.ShowSuccess($"{CreateRequest.Name} has been created successfully");
                 NavigationManager.NavigateTo("/todolist");
+            }
+            else
+            {
+                ToastService.ShowError("Unable to create new task");
             }
         }
     }
--- Tu_hoc_blazor_assembly/Tu_hoc_blazor_assembly/Program.cs
+++ Tu_hoc_blazor_assembly/Tu_hoc_blazor_assembly/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tu_hoc_blazor_assembly;
@@ -6,6 +7,7 @@ using Tu_hoc_blazor_assembly.Service;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped<ITaskAPIClient,TaskAPIClient>();
 builder.Services.AddScoped<IUserAPIClient,UserAPIClient>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7043") });
