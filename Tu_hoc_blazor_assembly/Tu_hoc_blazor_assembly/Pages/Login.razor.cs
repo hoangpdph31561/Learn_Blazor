8010ad5720c441b44dfd42ba628561bf44937fca
Hoàng Phạm Đức <hoang23577@gmail.com>
2023-09-13 21:56:10 +0700

    Create-form-login

---
+++ Tu_hoc_blazor_assembly/Tu_hoc_blazor_assembly/Pages/Login.razor.cs
@@ -0,0 +1,9 @@
+﻿using ToDoList_ViewModel;
+
+namespace Tu_hoc_blazor_assembly.Pages
+{
+    public partial class Login
+    {
+        private LoginRequest LoginRequest { get; set; } = new LoginRequest();
+    }
+}
